4d700d3a8ca0ef2c3e271a6bc8c0b374e04b21c6
catalindumitrache76 <catalindumitrache76@yahoo.es>
2016-11-23 22:48:16 +0100

    GUI Tests working on Tomcat with Localhost

--- src/test/java/tests/GUITest.java
+++ src/test/java/tests/GUITest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.ElementNotVisibleException;
 
 public class GUITest {
@@ -32,7 +33,7 @@ public class GUITest {
 	@Test
 	public void index(){
 		System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-		driver.get("http://pruebaopenshift-socialsport.rhcloud.com/Servidor/");	
+		driver.get("localhost:8080/Servidor/");	
 		driver.findElement(By.id("iniciar")).click();
 		System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
 	}
@@ -42,10 +43,10 @@ public class GUITest {
 	@Test
 	public void login(){
 		System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-		driver.get("http://pruebaopenshift-socialsport.rhcloud.com/Servidor/signup.html");	
+		driver.get("localhost:8080/Servidor/signup.html");	
 		driver.findElement(By.id("login-form-link")).click();
-		driver.findElement(By.id("emailL")).sendKeys("test@test.test");
-		driver.findElement(By.id("contrasenaL")).sendKeys("test");
+		driver.findElement(By.id("emailL")).sendKeys("catalindumitrache76@yahoo.es");
+		driver.findElement(By.id("contrasenaL")).sendKeys("thisisnotmyrealpassword");
 		driver.findElement(By.id("remember")).click();
 		driver.findElement(By.id("login-submit")).click();
 		System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
@@ -54,12 +55,11 @@ public class GUITest {
 	@Test
 	public void perfil(){
 		System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-		driver.get("http://pruebaopenshift-socialsport.rhcloud.com/Servidor/muro.html");	
+		login();	
 		driver.findElement(By.id("perfil")).click();
 		driver.findElement(By.id("mySportsButton")).click();
 		driver.findElement(By.id("myEventsButton")).click();
-		driver.findElement(By.id("creadosCrearEventoButton")).click();
-		driver.findElement(By.id("perfil")).click();
+		driver.findElement(By.id("mySportsButton")).click();
 		driver.findElement(By.id("myEventsButton")).click();
 		driver.findElement(By.id("verEventosButton")).click();
 		driver.findElement(By.id("perfil")).click();
@@ -68,19 +68,21 @@ public class GUITest {
 		driver.findElement(By.id("creaEvent")).click();
 		driver.findElement(By.id("nombre")).sendKeys("Nombre de prueba");
 		driver.findElement(By.id("hora")).sendKeys("Hora del evento");
-		driver.findElement(By.id("fecha")).sendKeys("Fecha del evento");
+		driver.findElement(By.id("fecha")).sendKeys("11/22/1111");
 		driver.findElement(By.id("descripcion")).sendKeys("Lorem ipsum dolor sit amet, "
 				+ "consectetur adipiscing elit. Integer eget lacus et massa vestibulum "
 				+ "scelerisque. Nulla ac leo sed orci egestas viverra ac a elit.");
-		driver.findElement(By.id("deporte")).click();
+		Select dropdown = new Select(driver.findElement(By.id("deporte")));
+		dropdown.selectByVisibleText("Baloncesto");
+		driver.findElement(By.id("creador")).sendKeys("catalindumitrache76@yahoo.es");
+		driver.findElement(By.id("event-submit")).click();
 		System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
 	}
 	
 	@Test
 	public void muroVacio(){
 		System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-		System.err.println("CUIDADO: TEST PROBADO CON TOMCAT EN LOCALHOST");
-		driver.get("http://pruebaopenshift-socialsport.rhcloud.com/Servidor/muro.html");	
+		login();
 		driver.findElement(By.className("navbar-brand")).click();
 		driver.findElement(By.id("search")).sendKeys("amigo1");
 		driver.findElement(By.id("buttonBuscar")).click();
@@ -115,7 +117,7 @@ public class GUITest {
 	public void registro() throws InterruptedException{
 		System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
 		System.err.println("CUIDADO: TEST PROBADO CON TOMCAT EN LOCALHOST");
-		driver.get("http://pruebaopenshift-socialsport.rhcloud.com/Servidor/signup.html");	
+		driver.get("localhost:8080/Servidor/signup.html");	
 		driver.findElement(By.id("register-form-link")).click();
 		driver.findElement(By.id("username")).sendKeys("test");
 		driver.findElement(By.id("emailR")).sendKeys("test@test.test");

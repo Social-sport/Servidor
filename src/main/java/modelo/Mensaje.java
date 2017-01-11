a560e1c892a6ff9d9fed61106b6e9659cbc107f6
Javier Murillo <jmurillo93@gmail.com>
2017-01-11 17:52:02 +0100

    comentarios mensaje y notificacion

--- src/main/java/modelo/Mensaje.java
+++ src/main/java/modelo/Mensaje.java
@@ -9,6 +9,15 @@ public class Mensaje {
 	private String autor;
 	private String destinatario;
 
+	 /**
+	  * Crea un nuevo objeto Mensaje que representa un mensaje
+	  * @param id Identificador del mensaje
+	  * @param fecha Fecha del mensaje
+	  * @param hora Hora del mensaje
+	  * @param texto Texto del mensaje
+	  * @param autor Autor del mensaje
+	  * @param destinatario Destinatario del mensaje
+	  */
 	public Mensaje(int id, String fecha, String hora, String texto,
 			String autor, String destinatario) {
 		this.id=id;
@@ -19,6 +28,14 @@ public class Mensaje {
 		this.destinatario=destinatario;
 	}
 
+	 /**
+	  * Crea un nuevo objeto Mensaje que representa un mensaje
+	  * @param fecha Fecha del mensaje
+	  * @param hora Hora del mensaje
+	  * @param texto Texto del mensaje
+	  * @param autor Autor del mensaje
+	  * @param destinatario Destinatario del mensaje
+	  */
 	public Mensaje(String fecha, String hora, String texto,
 			String autor, String destinatario) {
 		this.fecha=fecha;
@@ -27,51 +44,87 @@ public class Mensaje {
 		this.autor=autor;
 		this.destinatario=destinatario;
 	}
-
+	
+	/**
+	 * Devuelve el identificador del mensaje
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Establece el identificador del mensaje
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Devuelve la fecha del mensaje
+	 */
 	public String getFecha() {
 		return fecha;
 	}
 
+	/**
+	 * Establece la fecha del mensaje
+	 */
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
 
+	/**
+	 * Devuelve la hora del mensaje
+	 */
 	public String getHora() {
 		return hora;
 	}
 
+	/**
+	 * Establece la hora del mensaje
+	 */
 	public void setHora(String hora) {
 		this.hora = hora;
 	}
 
+	/**
+	 * Devuelve el texto del mensaje
+	 */
 	public String getTexto() {
 		return texto;
 	}
 
+	/**
+	 * Establece el texto del mensaje
+	 */
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
 
+	/**
+	 * Devuelve el autor del mensaje
+	 */
 	public String getAutor() {
 		return autor;
 	}
 
+	/**
+	 * Establece el autor del mensaje
+	 */
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
 
+	/**
+	 * Devuelve el destinatario del mensaje
+	 */
 	public String getDestinatario() {
 		return destinatario;
 	}
 
+	/**
+	 * Establece el destinatario del mensaje
+	 */
 	public void setDestinatario(String destinatario) {
 		this.destinatario = destinatario;
 	}
--- src/main/java/modelo/Notificacion.java
+++ src/main/java/modelo/Notificacion.java
@@ -13,6 +13,16 @@ public class Notificacion {
 	private String tipo;
 	private int idEvent;
 
+	 /**
+	  * Crea un nuevo objeto Mensaje que representa un mensaje
+	  * @param id Id de la notificación
+	  * @param usuarioEnvia Usuario que envia la notificación
+	  * @param usuarioRecibe Usuario que recibe la notificación
+	  * @param descripcion Descripción de la notificación
+	  * @param fecha Fecha de la notificación
+	  * @param hora Hora de la notificación
+	  * @param tipo Tipo de notificación
+	  */
 	public Notificacion(int id, String usuario, String descripcion, String fecha, String hora, String tipo) {
 		this.id=id;
 		this.usuarioEnvia=usuario;
@@ -22,6 +32,17 @@ public class Notificacion {
 		this.tipo=tipo;
 	}	
 
+	 /**
+	  * Crea un nuevo objeto Mensaje que representa un mensaje
+	  * @param usuarioEnvia Usuario que envia la notificación
+	  * @param usuarioRecibe Usuario que recibe la notificación
+	  * @param nombre Nombre de la notificación
+	  * @param foto Foto adjunta a la notificación
+	  * @param descripcion Descripción de la notificación
+	  * @param fecha Fecha de la notificación
+	  * @param hora Hora de la notificación
+	  * @param tipo Tipo de notificación
+	  */
 	public Notificacion(String usuarioEnvia, String usuarioRecibe, String nombre, String foto, String descripcion,
 			String fecha, String hora, String tipo) {
 		super();
@@ -35,9 +56,20 @@ public class Notificacion {
 		this.tipo = tipo;
 	}
 	
+	 /**
+	  * Crea un nuevo objeto Notificacion que representa una notificacion
+	  * @param usuarioEnvia Usuario que envia la notificación
+	  * @param usuarioRecibe Usuario que recibe la notificación
+	  * @param nombre Nombre de la notificación
+	  * @param foto Foto adjunta a la notificación
+	  * @param descripcion Descripción de la notificación
+	  * @param fecha Fecha de la notificación
+	  * @param hora Hora de la notificación
+	  * @param tipo Tipo de notificación
+	  * @param idEvent Id del evento asociado a la notificación
+	  */
 	public Notificacion(String usuarioEnvia, String usuarioRecibe, String nombre, String foto,
 			String descripcion, String fecha, String hora, String tipo, int idEvent) {
-	
 		this.usuarioEnvia = usuarioEnvia;
 		this.usuarioRecibe = usuarioRecibe;
 		this.nombre = nombre;
@@ -49,86 +81,146 @@ public class Notificacion {
 		this.idEvent = idEvent;
 	}
 
+	/**
+	 * Devuelve el Id del evento asociado a la notificacion
+	 */
 	public int getIdEvent() {
 		return idEvent;
 	}
 
+	/**
+	 * Establece el Id del evento asociado a la notificacion
+	 */
 	public void setIdEvent(int idEvent) {
 		this.idEvent = idEvent;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
+	/**
+	 * Devuelve el Id asociado a la notificación
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Establece el Id asociado a la notificación
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * Devuelve el nombre del usuario que envía la notificación
+	 */
 	public String getUsuarioEnvia() {
 		return usuarioEnvia;
 	}
 
+	/**
+	 * Establece el nombre del usuario que envía la notificación
+	 */
 	public void setUsuarioEnvia(String usuarioEnvia) {
 		this.usuarioEnvia = usuarioEnvia;
 	}
 
+	/**
+	 * Devuelve el nombre de la notificación
+	 */
 	public String getNombre() {
 		return nombre;
+	}
+	
+	/**
+	 * Establece el nombre de la notificación
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public void setnombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Devuelve el nombre del usuario que recibe la notificación
+	 */
 	public String getUsuarioRecibe() {
 		return usuarioRecibe;
 	}
 
+	/**
+	 * Establece el nombre del usuario que recibe la notificación
+	 */
 	public void setUsuarioRecibe(String usuarioRecibe) {
 		this.usuarioRecibe = usuarioRecibe;
 	}
 	
+	/**
+	 * Devuelve el nombre de la foto asociada a la notificación
+	 */
 	public String getFoto() {
 		return foto;
 	}
 
+	/**
+	 * Establece el nombre de la foto asociada a la notificación
+	 */
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
 
+	/**
+	 * Devuelve la descripción de la notificación
+	 */
 	public String getDescripcion() {
 		return descripcion;
 	}
 
+	/**
+	 * Establece la descripción de la notificación
+	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 
+	/**
+	 * Devuelve la fecha de la notificación
+	 */
 	public String getFecha() {
 		return fecha;
 	}
 
+	/**
+	 * Establece la fecha de la notificación
+	 */
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
 
+	/**
+	 * Devuelve la hora de la notificación
+	 */
 	public String getHora() {
 		return hora;
 	}
 
+	/**
+	 * Establece la hora de la notificación
+	 */
 	public void setHora(String hora) {
 		this.hora = hora;
 	}
 
+	/**
+	 * Devuelve el tipo de la notificación
+	 */
 	public String getTipo() {
 		return tipo;
 	}
 
+	/**
+	 * Establece el tipo de la notificación
+	 */
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
